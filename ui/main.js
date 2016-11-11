ec4f557aca1e0a149b0f4a02556cd2fef66f0910
PRASHANT KUMAR <pmishra2702@gmail.com>
2016-11-12 01:08:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,16 @@
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function(){
-    counter =counter + 1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreaystatechange = function(){
+        if(request.readyState === XMLHtppRequest.Done){
+            if(request.status === 200){
+                var counter = request.response.Text;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET', 'http://pmishra270.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
