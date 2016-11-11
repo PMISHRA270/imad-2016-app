d6b8d9877240e2995b409e96dd29a7f26c8a9be7
PRASHANT KUMAR <pmishra2702@gmail.com>
2016-11-12 02:39:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,11 +23,11 @@ submit.onclick=function(){
         if(request.readyState === XMLHttpRequest.Done){
             
         if(request.status===200){
-            var names=request.response.Text;
+            var names=request.responseText;
             names=JSON.parse(names);
     
             var list='';
-            for(var i=0;i<names.length;i++){
+            for(var i=0;i< names.length;i++){
             list += '<li>' + names[i] + '</li>';
         
          }
@@ -38,7 +38,7 @@ submit.onclick=function(){
 };
 
     
-var nameInput=documentGetElementById('name');
+var nameInput=document.getElementById('name');
 var name = nameInput.value;
 request.open('GET','http://pmishra270.imad.hasura-app.io/submit-name?name=' + name,true);
 request.send(null);
