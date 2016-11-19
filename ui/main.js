7b070190c1d1a040f5a2af774f22f19e940e09a7
PRASHANT KUMAR <pmishra2702@gmail.com>
2016-11-19 23:49:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,45 +1,124 @@
-var button=document.getElementById('counter');
+function loadLoginForm () {
+    var loginHtml = `
+        <h3>Login/Register to get more access</h3>
+        <input type="text" id="username" placeholder="username" />
+        <input type="password" id="password" />
+        <br/><br/>
+        <input type="submit" id="login_btn" value="Login" />
+        <input type="submit" id="register_btn" value="Register" />
+        `;
+    document.getElementById('login_area').innerHTML = loginHtml;
+    
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        var request = new XMLHttpRequest();
+        
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              if (request.status === 200) {
+                  submit.value = 'Login Sucessful';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+        };
+        
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
+    
+    var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        var request = new XMLHttpRequest();
+        
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
+}
 
-button.onclick=function(){
+function loadLoggedInUser (username) {
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
+    `;
+}
+
+function loadLogin () {
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML=counter.toString();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
             }
         }
     };
-    request.open('GET', 'http://pmishra270.imad.hasura-app.io/counter',true);
+    
+    request.open('GET', '/check-login', true);
     request.send(null);
-};
+}
 
-var submit=document.getElementById('submit_btn');
-submit.onclick=function(){
-    var request= new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        
-        if(request.readyState === XMLHttpRequest.DONE){
-            
-        if(request.status===200){
-            var names=request.responseText;
-            names=JSON.parse(names);
-    
-            var list='';
-            for(var i=0;i< names.length;i++){
-            list += '<li>' + names[i] + '</li>';
-        
-         }
-            var ul= document.getElementById('namelist');
-            ul.innerHTML=list;
+function loadArticles () {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>";
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!');
+            }
         }
-     }
-};
-
+    };
     
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-request.open('GET','http://pmishra270.imad.hasura-app.io/submit-name?name=' + name,true);
-request.send(null);
-};
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+}
+
+
+loadLogin();
+
+loadArticles();
